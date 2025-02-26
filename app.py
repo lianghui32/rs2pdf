04ab2b947901d797183cadf56fdf5a201bf23c3d
lh <2917759538@qq.com>
2025-02-27 04:04:00 +0800

    解决跨域

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, send_file
 from io import BytesIO
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/rs-pdf', methods=['GET'])
 def rs_pdf():
